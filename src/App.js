46c9c3b87e014524cf4a666cf4eff8a5a254a452
CGG <cggtsds59@gmail.com>
2023-07-20 17:26:59 +0530

    Commit 20-07-2023

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import {Routes,Route} from 'react-router-dom'
 import { Home } from './components/Home';
-import { About } from './components/About';
+
 import { Navbar } from './components/Navbar';
 import { OrderSummary } from './components/OrderSummary';
 import { PageNotFound } from './components/PageNotFound';
 import { Product } from './components/Product';
 import { FeaturedProducts } from './components/FeaturedProducts';
 import { NewProducts } from './components/NewProducts';
+import React from 'react';
+const LazyAbout=React.lazy(()=>import('./components/About'))
 function App() {
   return (
     <>
@@ -16,7 +18,7 @@ function App() {
     <Routes>
          <Route path='*' element={<PageNotFound/>}/>
          <Route path='/' element={<Home/>}/>
-         <Route path='about' element={<About/>}/>
+         <Route path='about' element={<React.Suspense fallback='Loading...'><LazyAbout/></React.Suspense>}/>
          <Route path='order-summary' element={<OrderSummary/>}/>
          <Route path='products' element={<Product/>}>
           <Route index element={<FeaturedProducts/>}/>
